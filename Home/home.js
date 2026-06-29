08732b104625b7aaaa3b490a4a07d7ba68084c75
Mohammad Sakib Hasan <mohdsakib6307@gmail.com>
2026-06-29 23:52:53 +0530

    Fix redirect path

--- Home/home.js
+++ Home/home.js
@@ -3,7 +3,7 @@ const user = JSON.parse(localStorage.getItem("user"));
 
 if(isLoggedIn !== "true"){
     alert("Please login first")
-    window.location.href="/login/login.html"
+    window.location.href="login/login.html"
 }
 
 const welcomeText = document.getElementById("welcomeText");
@@ -13,5 +13,5 @@ const logoutBtn = document.getElementById("logoutBtn");
 logoutBtn.addEventListener("click",function(){
     localStorage.removeItem("isLoggedIn");
     alert("Logout Successful");
-    window.location.href="/login/login.html";
+    window.location.href="login/login.html";
 });
